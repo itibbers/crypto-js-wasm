878062f206a601f980f38e6299ba54f5ef20c70c
itibbers <jxn2014@gmail.com>
2022-07-05 21:22:34 +0800

    feat: change name

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,33 +1,21 @@
-const path = require('path');
-const pkg = require('../package.json');
-const babel = require('@rollup/plugin-babel');
-
-
-const banner = `/*
-    @license
-    crypto-js v${pkg.version}
-    (c) 2009-2013 Jeff Most
-    (c) 2013-${new Date().getFullYear()} ${pkg.author.name}
-    ${pkg.repository.url}
-    Released under the MIT License.
-*/`;
+const path = require('path')
+const babel = require('@rollup/plugin-babel')
 
 const uniqResolve = (p) => {
-  return path.resolve(__dirname, './', p);
-};
+  return path.resolve(__dirname, './', p)
+}
 
 module.exports = {
   input: uniqResolve('../src/index.js'),
   output: {
     file: uniqResolve('../lib/index.js'),
     format: 'umd',
-    name: 'CryptoJSWasm',
-    banner
+    name: 'CryptoJSPro',
   },
   plugins: [
     babel.babel({
       exclude: 'node_modules/**',
-      plugins: ['@babel/plugin-proposal-class-properties']
+      plugins: ['@babel/plugin-proposal-class-properties'],
     }),
-  ]
-};
+  ],
+}
